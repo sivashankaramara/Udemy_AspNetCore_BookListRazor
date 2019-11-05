af46f0e92483185ed839a971fca55c905479d80a
unknown <sivashankar.amara@gmail.com>
2019-11-05 08:37:41 +0530

    Commited first version of Asp.Net core Book list razor on nov 5th 2019

---
+++ BookListRazor/BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //When ever any new models added to class we need to add those model here for DB interaction
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BookListRazor/BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CreateModel(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.Book.Add(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
